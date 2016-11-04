4171ee93a2881a3dde8fb2b6933ff20ededb5ede
Vincent Deloso <vincentdeloso@gmail.com>
2016-11-03 23:34:57 -0600

    Moved the layer rules logic from the Game Layer Manager into a separate utility class.

--- Assets/Scripts/GameLayerManager.cs
+++ Assets/Scripts/GameLayerManager.cs
@@ -43,41 +43,9 @@ public class GameLayerManager : View, IGameLayerManager
                 Collider collider = colorable.GetComponent<Collider>();
                 if (collider != null)
                 {
-                    collider.enabled = !MatchesLayerRules(e.Layer, colorable.layer);
+                    collider.enabled = !LayerUtils.MatchesLayerRules(e.Layer, colorable.layer);
                 }
             }
         }
-    }
-
-    private bool MatchesLayerRules(ColorLayer layer, int targetLayer)
-    {
-        bool matches = false;
-        string target = LayerMask.LayerToName(targetLayer);
-
-        switch (layer)
-        {
-            case ColorLayer.Red:
-                matches = target.Equals("Cyan");
-                break;
-            case ColorLayer.Green:
-                matches = target.Equals("Magenta");
-                break;
-            case ColorLayer.Blue:
-                matches = target.Equals("Yellow");
-                break;
-            case ColorLayer.Cyan:
-                matches = target.Equals("Red");
-                break;
-            case ColorLayer.Magenta:
-                matches = target.Equals("Green");
-                break;
-            case ColorLayer.Yellow:
-                matches = target.Equals("Blue");
-                break;
-            default:
-                break;
-        }
-
-        return matches;
     }
 }
---
+++ Assets/Scripts/LayerUtils.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class LayerUtils
+{
+
+    public static bool MatchesLayerRules(ColorLayer layer, int targetLayer)
+    {
+        bool matches = false;
+        string target = LayerMask.LayerToName(targetLayer);
+
+        switch (layer)
+        {
+            case ColorLayer.Red:
+                matches = target.Equals("Cyan");
+                break;
+            case ColorLayer.Green:
+                matches = target.Equals("Magenta");
+                break;
+            case ColorLayer.Blue:
+                matches = target.Equals("Yellow");
+                break;
+            case ColorLayer.Cyan:
+                matches = target.Equals("Red");
+                break;
+            case ColorLayer.Magenta:
+                matches = target.Equals("Green");
+                break;
+            case ColorLayer.Yellow:
+                matches = target.Equals("Blue");
+                break;
+            default:
+                break;
+        }
+
+        return matches;
+    }
+}
